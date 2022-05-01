9686af86b10fbeac0e63c51587ddf46bae6655dd
DoggoProgrammer <85500383+DoggoProgrammer@users.noreply.github.com>
2022-05-01 14:39:04 +0200

    Update main.lua

--- chat/main.lua
+++ chat/main.lua
@@ -7,5 +7,6 @@ end
 local chat = game.Players.LocalPlayer.PlayerGui.Chat.Frame.ChatChannelParentFrame.Frame_MessageLogDisplay.Scroller
 rconsoleclear()
 chat.ChildAdded:Connect(function(c)
+     wait(0.5)
      rconsoleprint(c.TextLabel.TextButton.Text .. ' ' .. c.TextLabel.Text)
 end)

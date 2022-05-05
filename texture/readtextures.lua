b17a7d5cb19801a01d298c862d2b73eae1a3865a
DoggoProgrammer <85500383+DoggoProgrammer@users.noreply.github.com>
2022-05-05 20:03:01 +0200

    Update readtextures.lua

--- texture/readtextures.lua
+++ texture/readtextures.lua
@@ -12,6 +12,9 @@ local Use = Instance.new("TextButton")
 local UICorner_4 = Instance.new("UICorner")
 local Nameofthecreator = Instance.new("TextLabel")
 local UICorner_5 = Instance.new("UICorner")
+local Texturepackloading = Instance.new("Frame")
+local Message = Instance.new("TextLabel")
+local Materials = {"Brick", "Cobblestone", "Concrete", "CorrodedMetal", "DiamondPlate", "Fabric", "Foil", "ForceField", "Glass", "Granite", "Grass", "Ice", "Marble", "Metal", "Neon", "Pebble", "Plastic", "Sand", "Slate", "SmoothPlastic", "Wood", "WoodPlanks"}
 
 Maingui.Name = "Maingui"
 Maingui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
@@ -104,14 +107,70 @@ Nameofthecreator.TextXAlignment = Enum.TextXAlignment.Left
 UICorner_5.CornerRadius = UDim.new(0, 4)
 UICorner_5.Parent = Nameofthecreator
 
+Texturepackloading.Name = "Texturepackloading"
+Texturepackloading.Parent = game.StarterGui.ScreenGui
+Texturepackloading.BackgroundColor3 = Color3.fromRGB(60, 60, 60)
+Texturepackloading.BorderColor3 = Color3.fromRGB(60, 60, 60)
+Texturepackloading.Size = UDim2.new(0, 930, 0, 455)
+Texturepackloading.Visible = false
+
+Message.Name = "Message"
+Message.Parent = Texturepackloading
+Message.BackgroundColor3 = Color3.fromRGB(90, 90, 90)
+Message.BorderColor3 = Color3.fromRGB(90, 90, 90)
+Message.Position = UDim2.new(0.0516129024, 0, 0.292307705, 0)
+Message.Size = UDim2.new(0, 837, 0, 199)
+Message.Font = Enum.Font.SourceSansBold
+Message.Text = "Loading Texturepack"
+Message.TextColor3 = Color3.fromRGB(255, 255, 255)
+Message.TextSize = 16.000
+Message.TextWrapped = true
+Message.TextTruncate = Enum.TextTruncate.AtEnd
+Message.TextXAlignment = Enum.TextXAlignment.Left
+Message.TextYAlignment = Enum.TextYAlignment.Top
+
 function gtp(txt)
+    local Facing = {"Back", "Bottom", "Front", "Left", "Right", "Top"}
+    local http = game:GetService('HttpService')
+    local output = ''
+    if txt:sub(1, 1) == '/' then
+        http = http:JSONDecode(readfile(txt))
+    else
+        http = http:JSONDecode(game:HttpGet(txt))
+    end
+    local function oa(ot)
+        output = output .. ot
+    end
+ ------------------------------------------------------------- the decal method
+    Texturepackloading.Visible = true
+    Message.Text = 'Loading Texturepack'
+    for _, v in pairs(Materials) do
+        for _, i in pairs(game:GetDescendants()) do
+            local success, errmsg = pcall(function()
+                 print(i.Material)
+            end)
+            if success then
+                for x = 1, 6 do
+                    local Decal = Instance.new("Decal")
+                    Decal.Parent = i
+                    Decal.Face = Enum.NormalId[Facing[x]]
+                    Decal.Texture = http[tostring(i.Material)]
+                end
+            end
+        end
+    end
+    Message.Text = 'Texturepack Loaded'
+    task.wait(3)
+    Texturepackloading.Visible = false
 end
+-------------------------------------------------------------
 Search.FocusLost:Connect(function()
      Search.PlaceholderColor3 = Color3.fromRGB(100, 100, 100)
      Search.PlaceholderText = 'Enter The Url or to search Files, type a / before the name'
      local txt = Search.Text
      local http = game:GetService('HttpService')
      if txt:sub(1, 1) == '/' then
+         print('Chose File')
          if isfile(txt) then
              local notp = Nameoftexturepack:Clone()
              notp.Parent = Results
@@ -121,6 +180,7 @@ Search.FocusLost:Connect(function()
                   gtp(txt)
              end)
          else
+             print('File was not Found')
              Search.Text = ''
              Search.PlaceholderColor3 = Color3.fromRGB(170, 0, 0)
              Search.PlaceholderText = 'That File was not found in workspace.'
@@ -130,8 +190,10 @@ Search.FocusLost:Connect(function()
               game:HttpGet(txt)
          end)
          if success then
+             print('Loading Texturepack')
              gtp(txt)
          else
+             print('Error whilst getting Texturepack from web')
              Search.Text = ''
              Search.PlaceholderColor3 = Color3.fromRGB(170, 0, 0)
              Search.PlaceholderText = errrmsg
